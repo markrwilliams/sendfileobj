7ec763da6a2db11e21669c2b271f08803d51e62f
Mark Williams <markrwilliams@gmail.com>
2014-06-04 00:55:33 -0700

    update examples + add README

--- examples/file_example.py
+++ examples/file_example.py
@@ -13,16 +13,16 @@ if __name__ == '__main__':
     args = argparser.parse_args()
 
     if args.send:
-        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        sock.connect(args.socket_path)
-        sendfileobj.sendfileobj(sock, open(args.send))
+        unix_domain_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        unix_domain_socket.connect(args.socket_path)
+        sendfileobj.sendfileobj(unix_domain_socket, open(args.send))
     else:
         if os.path.exists(args.socket_path):
             os.unlink(args.socket_path)
-        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        sock.bind(args.socket_path)
-        sock.listen(1)
-        connection, _ = sock.accept()
+        unix_domain_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        unix_domain_socket.bind(args.socket_path)
+        unix_domain_socket.listen(1)
+        connection, _ = unix_domain_socket.accept()
         recvd = sendfileobj.recvfileobj(connection)
         print repr(recvd)
         print recvd.read()
--- examples/tcp_example.py
+++ examples/tcp_example.py
@@ -13,21 +13,21 @@ if __name__ == '__main__':
     args = argparser.parse_args()
 
     if args.send:
-        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        sock.connect(args.socket_path)
+        unix_domain_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        unix_domain_socket.connect(args.socket_path)
 
-        conn = socket.create_connection((args.send, '80'))
-        conn.sendall('GET / HTTP/1.0\r\n'
+        http = socket.create_connection((args.send, '80'))
+        http.sendall('GET / HTTP/1.0\r\n'
                      '\r\n')
 
-        sendfileobj.sendfileobj(sock, conn)
+        sendfileobj.sendfileobj(unix_domain_socket, http)
     else:
         if os.path.exists(args.socket_path):
             os.unlink(args.socket_path)
-        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        sock.bind(args.socket_path)
-        sock.listen(1)
-        connection, _ = sock.accept()
+        unix_domain_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        unix_domain_socket.bind(args.socket_path)
+        unix_domain_socket.listen(1)
+        connection, _ = unix_domain_socket.accept()
 
         recvd = sendfileobj.recvfileobj(connection)
 
